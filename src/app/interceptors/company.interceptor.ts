1524d7de987f893e97450f382240d7dd07a7c30d
sebascifuentess <j.cifuentes@cincoveinticinco.com>
2025-04-10 16:42:07 -0500

    .

--- src/app/interceptors/company.interceptor.ts
+++ src/app/interceptors/company.interceptor.ts
@@ -2,7 +2,10 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const companyInterceptor: HttpInterceptorFn = (req, next) => {
 
-  if (req.method !== 'PUT' && ['getPresignedUrlService'].includes(req.url)) {
+  console.log(['getPresignedUrlService'].includes(req.url))
+  console.log(req)
+
+  if (req.method !== 'PUT' && req.url !== 'http://localhost:3000/finance/getPresignedUrlService' ) {
     const params = req.params.set('company_id', '18');
   
     const companyReq = req.clone({
